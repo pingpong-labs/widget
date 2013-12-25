3a4529cb3794204091b6934ab8a82676f95c78e6
Gravitano <gue.warsono@gmail.com>
2013-12-25 15:01:19 +0700

    Updated Commit

---
+++ WidgetServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Pingpong\Widget;
+
+use Illuminate\Support\ServiceProvider;
+use Exception;
+
+class WidgetServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('pingpong/widget', 'widget');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['widget'] = $this->app->share(function($app)
+		{
+			return new Widget;
+		});
+		$this->app->booting(function()
+		{
+			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+			$loader->alias('Widget', 'Pingpong\Widget\Facades\Widget');
+
+			$widgetFile = app_path('widget.php');
+			if(file_exists($widgetFile))
+			{
+				include $widgetFile;
+			}else
+			{
+				throw new Exception("Widget file not found!");
+			}
+		});
+		include __DIR__.'/../../routes.php';
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('widget');
+	}
+
+}
